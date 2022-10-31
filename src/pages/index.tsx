b89a3813eddccfc6e16c3ca69329209a40fed622
hota1024 <hotalog@hota1024.com>
2022-10-31 22:32:13 +0900

    :tada: (=^・・^=) < ininyalize

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - hota.works</title>
+      </Head>
+
+      <div>hota.works</div>
+    </>
+  )
+}
+
+export default HomePage
