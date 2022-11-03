f3e442a77dcf894d6f005f45e90aa144b6fcc8d1
hota1024 <hotalog@hota1024.com>
2022-11-03 12:26:43 +0900

    🚧add link

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,29 @@ const HomePage: NextPage = () => {
     <>
       <h1>ほたのポートフォリオになる予定のサイト</h1>
       <p>このサイトは現在開発中です。</p>
-      <div>
-        <a href="https://twitter.com/hota1024" target="_blank" rel="noreferrer">
-          Twitter
-        </a>
-      </div>
+      <section>
+        <h2>リンク</h2>
+        <ul>
+          <li>
+            <a
+              href="https://twitter.com/hota1024"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/hota1024"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
